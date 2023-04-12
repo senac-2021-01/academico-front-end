2d5e3022836f2ec06874c6b55712ae5985edbb65
Emanuel Ferreira da Silva <emanuelfs@weg.net>
2023-04-11 21:36:34 -0300

    Criando o formulário

--- App.js
+++ App.js
@@ -1,20 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {
+    createRef,
+    useState,
+} from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import axios from 'axios';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import {
+    Stack,
+    Button,
+    ActivityIndicator,
+} from "@react-native-material/core";
+
+import TextInput from "./src/components/TextInput";
+import { View } from "react-native";
+
+function App() {
+    const [loading, setLoading] = useState(false);
+
+    const textInputNameRef = createRef();
+    const textInputWorkloadRef = createRef();
+
+    const getTextInputNameRef = () => textInputNameRef.current;
+    const getTextInputWorkloadRef = () => textInputWorkloadRef.current;
+
+    const handleOnSaveButtonPress = async () => {
+        const auxTextInputNameRef = getTextInputNameRef();
+        const auxTextInputWorkloadRef = getTextInputWorkloadRef();
+
+        const textInputNameValue = auxTextInputNameRef.getValue();
+        const auxTextInputWorkloadValue = auxTextInputWorkloadRef.getValue();
+
+        const isTextInputNameValid = !!textInputNameValue;
+        const isTextInputWorkloadValid = !!auxTextInputWorkloadValue;
+
+        auxTextInputNameRef.setValid(isTextInputNameValid);
+        auxTextInputWorkloadRef.setValid(isTextInputWorkloadValid);
+
+        if (!!isTextInputNameValid && !!isTextInputWorkloadValid) {
+            setLoading(true);
+
+            try {
+                const { data } = await axios.post('http://192.168.43.44:8080/api/cursos', {
+                    nome: textInputNameValue,
+                    cargaHoraria: parseInt(auxTextInputWorkloadValue),
+                });
+
+                console.log(data);
+
+                setLoading(false);
+            } catch (error) {
+                console.error(error);
+
+                setLoading(false);
+            }
+        }
+    };
+
+    return (
+        <Stack
+            spacing={2}
+            style={{
+                margin: 20,
+                marginTop: 50,
+            }}
+        >
+            <TextInput
+                ref={textInputNameRef}
+                label="Nome"
+                variant="standard"
+            />
+            <TextInput
+                ref={textInputWorkloadRef}
+                label="Carga Horária"
+                variant="standard"
+            />
+            <View style={{ marginTop: 20 }}>
+                {!!loading ? <ActivityIndicator size="large" /> :
+                    <Button
+                        title="Salvar"
+                        onPress={handleOnSaveButtonPress}
+                    />
+                }
+            </View>
+        </Stack>
+    );
+};
+
+export default App;
---
+++ src/components/TextInput.js
@@ -0,0 +1,42 @@
+import React, {
+    forwardRef,
+    useImperativeHandle,
+    useState,
+} from "react";
+
+import {
+    TextInput as MaterialTextInput,
+} from "@react-native-material/core";
+
+const TextInput = forwardRef((props, ref) => {
+    const [value, setValue] = useState('');
+    const [valid, setValid] = useState(true);
+
+    const handleOnChangeText = text => {
+        if (!valid) {
+            setValid(true);
+        }
+
+        setValue(text);
+    };
+
+    const getValue = () => value;
+
+    useImperativeHandle(ref, () => ({
+        getValue,
+        setValid,
+    }));
+
+    return (
+        <MaterialTextInput
+            {...props}
+            value={value}
+            helperText={valid ? '' : props.helperText || 'Required field'}
+            onChangeText={handleOnChangeText}
+        />
+    );
+});
+
+TextInput.displayName = 'TextInput';
+
+export default TextInput;
